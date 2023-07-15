700f12926335bf51ce60e097dbb92f391a419e91
unknown <devr2407799@gmail.com>
2023-07-15 16:37:13 -0500

    AgregandoModelos

---
+++ app/Models/DetalleVenta.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DetalleVenta extends Model
+{
+    public $table='detalleventa';
+    protected $fillable=['idventas','iditinerario'];
+    protected $primaryKey=['idventas','iditinerario'];
+    public $timestamps=false;
+    use HasFactory;
+}
