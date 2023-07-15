0701c4e00f64b304d79ba1626236bbafd6b3d963
Brian Campos <bcampos@unitru.edu.pe>
2023-07-15 01:14:03 -0500

    agregando plantilla

---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('layouts.plantilla');
+});
+
+/* IGNORAR LA SIGUIENTE RUTA  */
+Route::get('/ejemplo', function () {
+    return view('ejemplo.ejemplo');
+});
+
+/* INICIO RUTAS MANTENIMIENTO */
+
+/* FIN RUTAS MANTENIMIENTO */
+
+
+
+/* INICIO RUTAS CONTABILIDAD */
+
+/* FIN RUTAS CONTABILIDAD */
+
+
+
+/* INICIO RUTAS RECURSOS HUMANOS */
+
+/* FIN RUTAS RECURSOS HUMANOS */
+
+
+
+/* INICIO RUTAS ENCOMIENDA */
+
+/* FIN RUTAS ENCOMIENDA */
+
+
