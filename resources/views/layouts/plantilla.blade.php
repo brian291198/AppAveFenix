dc221c1665aaa8b0e70aa2e8b737937b05660635
Brian Campos <bcampos@unitru.edu.pe>
2023-07-19 13:42:26 -0500

    agregando icono lado izquierdo superior

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -90,8 +90,7 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
       <a href="#" class="brand-link">
-        <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-          style="opacity: .8">
+        <img src="/logo_avefenix.png" class=" " alt="Logo Ave Fenix" style="width: 50px; margin:auto 10px; opacity: .8">
         <span class="brand-text font-weight-light">Ave Fenix</span>
       </a>
 
