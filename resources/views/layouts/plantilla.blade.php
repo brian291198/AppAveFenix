e942ba346de47ac52055355a0ab0fdc99d8221b9
@marcelo.samame <marcelo.samame@codingcrew.la>
2023-07-21 12:35:21 -0500

    encomienda links

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -290,7 +290,41 @@
 
 
         {{-- INICIO SUBSISTEMA ENCOMIENDA --}}
-
+        <li class="nav-item">
+          <a href="#" class="nav-link">
+            <i class="nav-icon fas fa-tachometer-alt"></i>
+            <p>
+              Encomiendas
+              <i class="right fas fa-angle-left"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="#" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Paquetes</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="/promociones" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Promociones</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="/tarifas" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Tarifas</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="/transportes" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Transportes</p>
+              </a>
+            </li>
+          </ul>
+        </li>
         {{-- FIN SUBSISTEMA ENCOMIENDA --}}
 
 
